4e94a64e3d6d8bc05636d0ae885333d2748c745f
davidbeleznay <dbeleznay09@gmail.com>
2025-12-13 09:04:01 -0800

    Create funding templates module structure

---
+++ funding_templates/__init__.py
@@ -0,0 +1,8 @@
+"""
+Funding Templates Module
+Provides template-based question generation and smart checklists for funding applications
+"""
+
+from .template_engine import FundingTemplate, TemplateManager
+
+__all__ = ['FundingTemplate', 'TemplateManager']
